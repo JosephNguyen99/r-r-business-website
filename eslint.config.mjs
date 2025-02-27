3e95e4bc3f4ea1134e4e764ab02235cf2d7c6e3d
Joseph Nguyen <jnguyen16@txtav.com>
2025-02-27 12:23:24 -0600

    updating linting issues.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+// import prettierPlugin from "eslint-plugin-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals"),
+//   {
+//     plugins: {
+//       prettier: prettierPlugin,
+//     },
+//     rules: {
+//       "react/no-unescaped-entities": "error",
+//       "prettier/prettier": "error",
+//     },
+    
+//     // plugins: {
+//     //   prettier: pluginObject
+//     // },
+//   },
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
+
+// eslint.config.mjs
+export default {
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    semi: "off",
+    quotes: "off"
+  }
+};
